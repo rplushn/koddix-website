4ca58719c5287fc4e24b1401064bb6c68b796a77
rplushn <rplushn@gmail.com>
2026-01-12 22:08:04 -0600

    fix: change About import to named import to fix build error

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,5 @@
 import { Background } from "@/components/background";
-import About from "@/components/blocks/about";
+import { About } from "@/components/blocks/about";
 import { AboutHero } from "@/components/blocks/about-hero";
 import { Investors } from "@/components/blocks/investors";
 import { DashedLine } from "@/components/dashed-line";
